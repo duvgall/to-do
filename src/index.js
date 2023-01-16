2b358cdac530c3792a988e4608f005ec3a5c25b4
Alan Doyle <alanduvgall@gmail.com>
2023-01-16 06:09:58 +0000

    initial commit take two

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import _ from 'lodash';
+import xxxx from './print.js';
+
+function createTask() {
+    
+    
+}
+
+
+
+function showTaskInput() {
+    //input under task-list appears
+    //get element by Id and then change display to block
+}
+
+function showProjectInput() {
+    //input under project-list appears
+}
+
+function addTask() {
+    //take text from input when add button is clicked and make it a new button entry in the list
+    //will need to get elements by Id, input and list
+    //input div to disappear again
+}
+
+function addProject() {
+        //take text from input when add button is clicked and make it a new button entry in the list
+        //will need to get elements by Id, input and list
+        //input div to disappear again
+
+}
+
+function cancelAdd(){
+    //make input div disappear again
+    //get element by Id and change display to none
+}
+
+function deleteTask() {
+    //remove li element from the ul
+}
+
+function deleteProject() {
+    //remove li element from the ul
+}
