4549de566a517c4989d34353128cce3c2489924c
Alan Doyle <alanduvgall@gmail.com>
2023-01-23 09:50:04 +0000

    change list display and add new task + project functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import xxxx from './print.js';
+import changeTaskInputDisplay() from './print.js';
 
 function createTask() {
     
@@ -8,25 +8,38 @@ function createTask() {
 
 
 
-function showTaskInput() {
-    //input under task-list appears
-    //get element by Id and then change display to block
-}
 
-function showProjectInput() {
-    //input under project-list appears
+function changeProjectInputDisplay() {
+    let projectInput = document.getElementById('project-container');
+    if (projectInput.style.display === 'none') {
+        projectInput.style.display === 'block'
+    } else {
+        projectInput.style.display === 'none'
+    };
 }
 
 function addTask() {
-    //take text from input when add button is clicked and make it a new button entry in the list
-    //will need to get elements by Id, input and list
-    //input div to disappear again
+    let newTaskText = document.getElementById('task-user-input').textContent;         //take text from input when add button is clicked and make it a new button entry in the list
+    let taskList = document.getElementById('task-list');
+    let newTaskBtn = document.createElement('button');
+    let newListElement = document.createElement('li');
+    taskList.appendChild(newListElement);
+    newTaskBtn.textContent = newTaskText; 
+    newListElement.appendChild(newTaskBtn);
+    newTaskText.textContent = '';
+    changeTaskInputDisplay();                                                     //input div to disappear again
 }
 
 function addProject() {
-        //take text from input when add button is clicked and make it a new button entry in the list
-        //will need to get elements by Id, input and list
-        //input div to disappear again
+    let newProjectText = document.getElementById('project-user-input').textContent;         //take text from input when add button is clicked and make it a new button entry in the list
+    let projectList = document.getElementById('project-list');
+    let newProjectBtn = document.createElement('button');
+    let newListElement = document.createElement('li');
+    projectList.appendChild(newListElement);
+    newProjectBtn.textContent = newProjectText; 
+    newListElement.appendChild(newProjectBtn);
+    newProjectText.textContent = '';
+    changeProjectInputDisplay();
 
 }
 
--- src/print.js
+++ src/print.js
@@ -1,3 +1,13 @@
-export default function xxxxxx() {
+export default function changeTaskInputDisplay() {
     
+}
+
+function changeTaskInputDisplay() {
+    let taskInput = document.getElementById('task-container');
+
+    if (taskInput.style.display === 'none') {
+    taskInput.style.display === 'block'
+    } else {
+        taskInput.style.display === 'none'
+    };
 }
